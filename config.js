904cf1c28cdacf322f6eb0dfecf775335a24dbfb
WEB KAS ASTREL <firdausmaulana090424@gmail.com>
2026-08-06 09:13:09 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+// ==========================================
+// FILE KONFIGURASI TERPISAH (ASTRAVELIS CONFIG)
+// ==========================================
+
+window.APP_CONFIG = {
+    // Tautan backend Google Apps Script
+    scriptURL: 'https://script.google.com/macros/s/AKfycbw9HX9zRlkd7FUXJfAjOpN8mn4Xn6FuwoaWekoAgkXMUsWBXeZ6abtSkxeJe6bQH0Th/exec',
+    
+    // Nominal Iuran per bulan
+    nominalIuran: 10000,
+
+    // Tautan Penting Kelas (Quick Links & Google Drive)
+    links: {
+        zoom: "https://us06web.zoom.us/j/4634019789?pwd=A1Mh6ctFpz0abaJsbkDxxisjZoowVb.1",
+        lms: "https://lms.maniclotim.sch.id/",
+        cbt: "https://cbt.akademiksistem.my.id/",
+        driveMoment: "https://drive.google.com/drive/folders/1xYiNCGUPes8LU2kJsKFFLTdGrFRTYXU0?usp=sharing",
+        driveMateri: "https://drive.google.com/drive/folders/10tKD3rfBM5zUlNKjmgxngLJkD3JzQS8S?usp=sharing"
+    },
+
+    // PIN Pengurus Default (Bisa diubah oleh pengurus via web & tersimpan di browser)
+    defaultRoles: {
+        "0101": { name: "Admin", access: ["uangKas", "cekKas", "moment", "materi"] },
+        "2222": { name: "Bendahara", access: ["uangKas", "cekKas"] },
+        "3333": { name: "Sekretaris", access: ["moment", "materi"] },
+        "4444": { name: "Ketua/Wakil Kelas", access: ["moment", "materi"] }
+    }
+};
