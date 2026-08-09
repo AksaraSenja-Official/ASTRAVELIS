e6433db7ed09a35ed6b71e093148c4117400931a
WEB KAS ASTREL <firdausmaulana090424@gmail.com>
2026-08-10 01:37:09 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -13,18 +13,18 @@ window.APP_CONFIG = {
     links: {
         zoom: "https://us06web.zoom.us/j/4634019789?pwd=A1Mh6ctFpz0abaJsbkDxxisjZoowVb.1",
         lms: "https://lms.maniclotim.sch.id/",
-        lms2: "https://sites.google.com/view/maniclotim-ski-lms/lms", // TAMBAHAN LMS 2
+        lms2: "https://sites.google.com/view/maniclotim-ski-lms/lms",
         cbt: "https://cbt.akademiksistem.my.id/",
         driveMoment: "https://drive.google.com/drive/folders/1xYiNCGUPes8LU2kJsKFFLTdGrFRTYXU0?usp=sharing",
         driveMateri: "https://drive.google.com/drive/folders/10tKD3rfBM5zUlNKjmgxngLJkD3JzQS8S?usp=sharing",
         instagramClass: "https://www.instagram.com/astravelis__?igsh=MWRlbG5haXR5bmV2NQ=="
     },
 
-    // PIN Pengurus Default (Dapat dikelola di Panel Admin)
+    // PIN Pengurus Default (Akses 'adminPanel' dibuka untuk Sekretaris & Ketua/Wakil Kelas)
     defaultRoles: {
         "0101": { name: "Admin", access: ["uangKas", "cekKas", "moment", "materi", "adminPanel"] },
         "2222": { name: "Bendahara", access: ["uangKas", "cekKas"] },
-        "3333": { name: "Sekretaris", access: ["moment", "materi"] },
-        "4444": { name: "Ketua/Wakil Kelas", access: ["moment", "materi"] }
+        "3333": { name: "Sekretaris", access: ["moment", "materi", "adminPanel"] },
+        "4444": { name: "Ketua/Wakil Kelas", access: ["moment", "materi", "adminPanel"] }
     }
 };
