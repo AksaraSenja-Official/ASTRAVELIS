ee1197e0a6a9acd52d59c4d9b1a63bdbe3d09255
WEB KAS ASTREL <firdausmaulana090424@gmail.com>
2026-08-11 09:09:14 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -6,22 +6,26 @@ window.APP_CONFIG = {
     // Tautan backend Google Apps Script
     scriptURL: 'https://script.google.com/macros/s/AKfycbw9HX9zRlkd7FUXJfAjOpN8mn4Xn6FuwoaWekoAgkXMUsWBXeZ6abtSkxeJe6bQH0Th/exec',
     
-    // Nominal Iuran per bulan
+    // Nominal Iuran Kas per bulan (Rp 10.000)
     nominalIuran: 10000,
 
-    // Tautan Penting Kelas (Quick Links & Google Drive & Media Sosial)
+    // Tautan Penting Kelas (Quick Links, SI-ASRAM, Google Drive & Media Sosial)
     links: {
         zoom: "https://us06web.zoom.us/j/4634019789?pwd=A1Mh6ctFpz0abaJsbkDxxisjZoowVb.1",
         lms: "https://lms.maniclotim.sch.id/",
         lms2: "https://sites.google.com/view/maniclotim-ski-lms/lms",
         cbt: "https://cbt.akademiksistem.my.id/",
+        
+        // Kompatibilitas ganda (camelCase & lowercase) agar tidak error saat dipanggil index.html
         siAsram: "https://sites.google.com/guru.paud.belajar.id/keasramaanmaniclomboktimur/si-asram?authuser=0",
+        siasram: "https://sites.google.com/guru.paud.belajar.id/keasramaanmaniclomboktimur/si-asram?authuser=0",
+        
         driveMoment: "https://drive.google.com/drive/folders/1xYiNCGUPes8LU2kJsKFFLTdGrFRTYXU0?usp=sharing",
         driveMateri: "https://drive.google.com/drive/folders/10tKD3rfBM5zUlNKjmgxngLJkD3JzQS8S?usp=sharing",
         instagramClass: "https://www.instagram.com/astravelis__?igsh=MWRlbG5haXR5bmV2NQ=="
     },
 
-    // PIN Pengurus Default (Akses 'adminPanel' dibuka untuk Sekretaris & Ketua/Wakil Kelas)
+    // PIN Pengurus Default & Hak Akses Fitur
     defaultRoles: {
         "0101": { name: "Admin", access: ["uangKas", "cekKas", "moment", "materi", "adminPanel"] },
         "2222": { name: "Bendahara", access: ["uangKas", "cekKas"] },
